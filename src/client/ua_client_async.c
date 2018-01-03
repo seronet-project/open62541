892b4db030cf2054168c724b1328bfee75897330
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2018-01-03 08:30:01 +0100

    Fix memory leak

--- src/client/ua_client_async.c
+++ src/client/ua_client_async.c
@@ -14,50 +14,51 @@
 #ifdef UA_ENABLE_SUBSCRIPTIONS
 
 void callback_PublishResponce(UA_Client *client, void *userdata,
-	UA_UInt32 requestId, const void *response)
+    UA_UInt32 requestId, const void *response)
 {
-	UA_PublishResponse* resp = (UA_PublishResponse*)response;
-	UA_PublishRequest* requ = (UA_PublishRequest*)userdata;
-	UA_Client_processPublishResponse(client, requ, resp);
-	
-	UA_PublishRequest_deleteMembers(requ);
+    UA_PublishResponse* resp = (UA_PublishResponse*)response;
+    UA_PublishRequest* requ = (UA_PublishRequest*)userdata;
+    UA_Client_processPublishResponse(client, requ, resp);
+    
+    UA_PublishRequest_deleteMembers(requ);
+    UA_PublishRequest_delete(requ);
 }
 
 UA_StatusCode
 UA_Client_Subscriptions_manuallySendPublishRequest_Async(UA_Client *client) {
-	if (client->state < UA_CLIENTSTATE_SESSION)
-		return UA_STATUSCODE_BADSERVERNOTCONNECTED;
+    if (client->state < UA_CLIENTSTATE_SESSION)
+        return UA_STATUSCODE_BADSERVERNOTCONNECTED;
 
-	UA_StatusCode retval = UA_STATUSCODE_GOOD;
+    UA_StatusCode retval = UA_STATUSCODE_GOOD;
 
-	UA_PublishRequest request;
-	UA_PublishRequest_init(&request);
-	request.subscriptionAcknowledgementsSize = 0;
+    UA_PublishRequest *request = UA_PublishRequest_new();
+    UA_PublishRequest_init(request);
+    request->subscriptionAcknowledgementsSize = 0;
 
-	UA_Client_NotificationsAckNumber *ack;
-	LIST_FOREACH(ack, &client->pendingNotificationsAcks, listEntry)
-		++request.subscriptionAcknowledgementsSize;
-	if (request.subscriptionAcknowledgementsSize > 0) {
-		request.subscriptionAcknowledgements = (UA_SubscriptionAcknowledgement*)
-			UA_malloc(sizeof(UA_SubscriptionAcknowledgement) * request.subscriptionAcknowledgementsSize);
-		if (!request.subscriptionAcknowledgements)
-			return UA_STATUSCODE_BADOUTOFMEMORY;
-	}
+    UA_Client_NotificationsAckNumber *ack;
+    LIST_FOREACH(ack, &client->pendingNotificationsAcks, listEntry)
+        ++request->subscriptionAcknowledgementsSize;
+    if (request->subscriptionAcknowledgementsSize > 0) {
+        request->subscriptionAcknowledgements = (UA_SubscriptionAcknowledgement*)
+            UA_malloc(sizeof(UA_SubscriptionAcknowledgement) * request->subscriptionAcknowledgementsSize);
+        if (!request->subscriptionAcknowledgements)
+            return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
 
-	int i = 0;
-	LIST_FOREACH(ack, &client->pendingNotificationsAcks, listEntry) {
-		request.subscriptionAcknowledgements[i].sequenceNumber = ack->subAck.sequenceNumber;
-		request.subscriptionAcknowledgements[i].subscriptionId = ack->subAck.subscriptionId;
-		++i;
-	}
+    int i = 0;
+    LIST_FOREACH(ack, &client->pendingNotificationsAcks, listEntry) {
+        request->subscriptionAcknowledgements[i].sequenceNumber = ack->subAck.sequenceNumber;
+        request->subscriptionAcknowledgements[i].subscriptionId = ack->subAck.subscriptionId;
+        ++i;
+    }
 
-	UA_UInt32 reqId;
-	retval = UA_Client_AsyncService_publish(client, request, callback_PublishResponce, &request, &reqId);
+    UA_UInt32 reqId;
+    retval = UA_Client_AsyncService_publish(client, *request, callback_PublishResponce, request, &reqId);
 
-	if (client->state < UA_CLIENTSTATE_SESSION)
-		return UA_STATUSCODE_BADSERVERNOTCONNECTED;
+    if (client->state < UA_CLIENTSTATE_SESSION)
+        return UA_STATUSCODE_BADSERVERNOTCONNECTED;
 
-	return retval;
+    return retval;
 }
 
 #endif /* UA_ENABLE_SUBSCRIPTIONS */
